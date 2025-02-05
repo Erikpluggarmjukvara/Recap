5221bfa55522f813356bc0bed9b86aac4e1e9f3f
Erik <81eriekd@gafe.molndal.se>
2025-02-05 13:19:45 +0100

    exercise 9 compleat

--- Models/Subscriber.cs
+++ Models/Subscriber.cs
@@ -1,9 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Newsletter.Models;
 
 public class Subscriber
 {
+    [BsonId] // Marks this property as the document's primary key
+    [BsonRepresentation(BsonType.ObjectId)] // Stores the Id as an ObjectId in MongoDB
     public string? Id { get; set; }
     [Required]
     public string? Name { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-using Newsletter.Services;
+
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Newsletter.Configurations;
 using Newsletter.Data;
+using Newsletter.Models;
+using Newsletter.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +15,33 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication("Cookies")
     .AddCookie(options => options.LoginPath = "/Account/Login");
 
-// Register the subscriber repository in the DI container
-builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+// Register the newsletter service in the DI container
+builder.Services.AddSingleton<INewsletterService, NewsletterService>();
 
-builder.Services.AddScoped<INewsletterService, NewsletterService>();
+// Insert the correct database repository based on the configuration
+var databaseToUse = builder.Configuration["DatabaseToUse"];
+
+switch (databaseToUse)
+{
+    // Add the InMemoryDb repository if the configuration is set to InMemoryDb or if the configuration is not set
+    case "InMemoryDb":
+    case var db when string.IsNullOrEmpty(db):
+        builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+        break;
+    case "MongoDb":
+        builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+        builder.Services.AddSingleton<ISubscriberRepository, MongoDbSubscriberRepository>(config =>
+        {
+            var settings = config.GetRequiredService<IOptions<MongoDBSettings>>().Value;
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            var collection = database.GetCollection<Subscriber>(settings.CollectionName);
+            return new MongoDbSubscriberRepository(collection);
+        });
+        break;
+    default:
+        throw new InvalidOperationException("Invalid database configuration");
+}
 
 var app = builder.Build();
 
@@ -36,6 +65,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
